18c1d4a0d63f90bbd23d377ae0eb44ed677f3263
Slava Richkov <v.v.richkov@yandex.ru>
2022-09-13 12:24:59 +0500

    fix

--- src/components/App.js
+++ src/components/App.js
@@ -60,7 +60,7 @@ function App() {
 
   useEffect(() => {
     if (
-      [isEditAvatarPopupOpen, isAddPlacePopupOpen, isDeletePlacePopupOpen, isEditProfilePopupOpen].includes === false
+      [isEditAvatarPopupOpen, isAddPlacePopupOpen, isDeletePlacePopupOpen, isEditProfilePopupOpen].includes !== false
       || isImagePopupOpened !== null
     ) {
       document.addEventListener('click', handleCloseAllPopupsClickOverlay);
