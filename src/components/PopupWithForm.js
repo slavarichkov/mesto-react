8c49a82acf17c447766b513dc9a5148d0819607d
Slava Richkov <v.v.richkov@yandex.ru>
2022-09-14 00:23:43 +0500

    refactoring

--- src/components/PopupWithForm.js
+++ src/components/PopupWithForm.js
@@ -4,7 +4,7 @@ function PopupWithForm({name, isOpen, isClose, text, children}) {
 
     return (
 
-        < div className={`popup ${name} popup_overlay ${isOpen}`}>
+        < div className={`popup ${name} popup_overlay ${isOpen? 'popup_open' : ''}`}>
             <div className="popup__content">
                 <button className="popup__close-button" type="button" onClick={isClose} />
                 <form className="popup__form" name={`${name}`} onSubmit={isClose} noValidate>
